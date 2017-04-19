67c9a9ba9709ab5dfc1f68fd940f7bf12a96b329
Lucas Kolstad <lkolstad@uw.edu>
2017-04-18 19:56:36 -0700

    setup.py changes to support uploading to pypi as 0.2.0 release

--- README.md
+++
@@ -1,9 +0,0 @@
-# rBCM - robust Bayesian Committee Machine
-
-This package implements a robust Bayesian Committee Machine Regressor in the
-style of a `sklearn` model.
-
-You can [read the docs here.](http://rbcm.readthedocs.io/en/latest/introduction.html)
-
-You can look at a minimal example of usage in the
-[notebooks.](https://github.com/lucaskolstad/rBCM/tree/master/notebooks/minimal.ipynb)
--- setup.py
+++ setup.py
@@ -2,22 +2,43 @@
 
 from setuptools import setup, find_packages
 
-install_reqs = []
-with open('requirements.txt') as f:
-    install_reqs.append(f.read().splitlines())
+
+def get_install_reqs():
+    install_reqs = []
+    with open('requirements.txt') as f:
+        install_reqs.append(f.read().splitlines())
+    return install_reqs
+
+
+def get_long_description():
+    with open('README.rst') as f:
+        desc = f.read()
+    return desc
 
 
 def setup_package():
     setup(
         name="rBCM",
-        description="A sample robust Bayesian Committee Machine \
-                     implementation",
+        description="A robust Bayesian Committee Machine Regressor.",
         author="Lucas J. Kolstad",
         author_email="lkolstad@uw.edu",
         version="0.2.0",
-        license="See LICENSE file.",
+        license="MIT",
         packages=find_packages(),
-        install_requires=install_reqs
+        install_requires=get_install_reqs(),
+        keywords="statistics gaussian process bayesian regression",
+        long_description=get_long_description(),
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: Unix",
+            "Programming Language :: Python",
+            "Topic :: Scientific/Engineering :: Artificial Intelligence",
+            "Topic :: Scientific/Engineering :: Information Analysis",
+            "Topic :: Scientific/Engineering :: Mathematics",
+        ],
     )
 
 
